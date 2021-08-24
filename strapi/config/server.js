b123c7b08e0e68a17b47ceb2b4eea6f31f525b6f
Helmi Pradita <mr.hellmii@gmail.com>
2021-08-24 11:12:33 +0700

    finish strapi with postgreSQL

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f1799d8282bd92655e4aa0aedf31d096'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e2828721-6e99-40be-adf0-5fd46c60430a'
+};
